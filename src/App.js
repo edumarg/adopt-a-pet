2710438985121022576269ba6065f34e3f880122
Eduardo Margulis <edumarg@gmx.com>
2022-05-20 10:28:16 +0300

    change background and added background attribution

--- src/App.js
+++ src/App.js
@@ -12,42 +12,52 @@ import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-      <header>
-        <Link to="/home" className="main-title">
-          Adopt a Me!
-        </Link>
-      </header>
-      <PetsContextProvider>
-        <PaginationContextProvider>
-          <Routes>
-            <Route path="/home" element={<Search />} />
-            <Route
-              path="/details/:id"
-              element={
-                <ErrorBoundary>
-                  <Details />{" "}
-                </ErrorBoundary>
-              }
-            ></Route>
-            <Route path="/not-found" element={<NotFound />} />
-            <Route exact path="/" element={<Navigate replace to="/home" />} />
-            <Route path="*" element={<Navigate replace to="/not-found" />} />
-          </Routes>
-        </PaginationContextProvider>
-      </PetsContextProvider>
-    </BrowserRouter>
+    <React.Fragment>
+      <BrowserRouter>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <header>
+          <Link to="/home" className="main-title">
+            Adopt a Me!
+          </Link>
+        </header>
+        <PetsContextProvider>
+          <PaginationContextProvider>
+            <Routes>
+              <Route path="/home" element={<Search />} />
+              <Route
+                path="/details/:id"
+                element={
+                  <ErrorBoundary>
+                    <Details />{" "}
+                  </ErrorBoundary>
+                }
+              ></Route>
+              <Route path="/not-found" element={<NotFound />} />
+              <Route exact path="/" element={<Navigate replace to="/home" />} />
+              <Route path="*" element={<Navigate replace to="/not-found" />} />
+            </Routes>
+          </PaginationContextProvider>
+        </PetsContextProvider>
+      </BrowserRouter>
+      <footer>
+        <p>
+          Background from{" "}
+          <a href="http://pets-images.dev-apis.com/pets/wallpaperB.jpg">
+            pets-images.dev-apis.com
+          </a>
+        </p>
+      </footer>
+    </React.Fragment>
   );
 }
 
